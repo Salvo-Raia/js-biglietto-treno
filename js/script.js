f4a8b2f0e24f85fac74473237c3dd9e52a972aa2
Salvo-Raia <salvo.raia@outlook.it>
2025-12-16 17:49:45 +0100

    Add standard ticket price

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 console.log("JS Ready to go");
 
 /* Il programma dovrà chiedere all'utente il numero di chilometri 
-che vuole percorrere e l'età del passeggero */ 
+   che vuole percorrere e l'età del passeggero */ 
 
 const userTravelLenght = parseInt(prompt("Quanti chilometri percorrerai?"));
 console.log("Chilometri selezionati:", userTravelLenght, typeof userTravelLenght);
 
 const userAge = parseInt(prompt("Quanti anni hai?"));
 console.log("Età utente:", userAge, typeof userAge);
+
+/*Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+  il prezzo del biglietto è definito in base ai km (0.21 € al km) */
+
+  const ticketStandardPrice = userTravelLenght * 0.21; 
+  console.log("Il prezzo base del biglietto è di:", ticketStandardPrice, "€", typeof ticketStandardPrice);
